7d1092b25467fbd43ee0456611d9ce1bb3278bb6
Voiislav <vojmitrovic@gmail.com>
2024-01-20 19:14:46 +0100

    feat: add protected routes of users and movies

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
+
 const mongoose = require('mongoose');
 
-const { PORT = 3000 } = process.env;
+const ErrorNotFound = require('./errors/ErrorNotFound');
+
+const auth = require('./middlewares/auth');
 
 const app = express();
+
+app.use(auth);
+
+app.use('/users', require('./routes/users'));
+
+app.use('/movies', require('./routes/movies'));
+
+app.use((req, res, next) => {
+  next(new ErrorNotFound('Маршрут не найден'));
+});
 
 mongoose.connect('mongodb://localhost:27017//bitfilmsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
