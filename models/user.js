8c5e8bfc0baa51d2679554b7cce651ac2a9094d3
Voiislav <vojmitrovic@gmail.com>
2024-01-20 15:49:59 +0100

    feat: add models and schemas for user and movies

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: 'Некорректный формат электронной почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
