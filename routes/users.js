e475ed63da29265474a34da73b1c7e56b4810426
Voiislav <vojmitrovic@gmail.com>
2024-01-20 18:00:04 +0100

    feat: add user routes

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+
+const { getCurrentUser, updateProfile } = require('../controllers/users');
+
+router.get('/me', getCurrentUser);
+router.patch('/me', updateProfile);
